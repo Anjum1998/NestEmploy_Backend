c1aba9c8bfe456ebfb6b17ed1a5a81a2dd817cf4
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-11 19:33:42 +0530

    first commit

---
+++ src/main/java/com/example/NestEmployApp_BackEnd/NestEmployAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.NestEmployApp_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestEmployAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestEmployAppBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NestEmployApp_BackEnd/NestEmployAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NestEmployApp_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestEmployAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
